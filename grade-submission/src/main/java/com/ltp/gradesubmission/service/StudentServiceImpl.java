bdf519fad3f224a0fae59f8d02b60bd012d775db
Ozi <oguzkaanyildiz@mac-ozi.local>
2023-01-01 11:17:33 +0100

    Save a student

--- grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -2,9 +2,17 @@ package com.ltp.gradesubmission.service;
 
 import java.util.List;
 
-import com.ltp.gradesubmission.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.StudentRepository;
+
+@Service
 public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @Override
     public Student getStudent(Long id) {
@@ -13,7 +21,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        return studentRepository.save(student);
     }
 
     @Override
@@ -24,6 +32,4 @@ public class StudentServiceImpl implements StudentService {
     public List<Student> getStudents() {
         return null;
     }
-
-
 }
